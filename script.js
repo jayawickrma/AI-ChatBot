a2e69979958cf13229af4877773433d7a350dc90
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-16 23:13:11 +0530

    linked api

--- script.js
+++ script.js
@@ -6,15 +6,28 @@ let userMessege;
 const createChatLi =(message ,className) =>{
     const chatLi =document.createElement("li")
     chatLi.classList.add("chat",className);
-    let chatContent =className === "outgoing" ?' <p>${message}</p> ':
+    let ChatContent =className === "outgoing" ?' <p>${message}</p> ':
        ' <span class="material -symbols-outlined">smart_toy</span><p>${message}</p>';
-        chatLi.innerHTML=chatContent;
-        return chatLi
+        chatLi.innerHTML=ChatContent;
+        return chatLi;
+}
+const generateResponce =() =>{
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+
+    const requestOptions={
+        method:"POST",
+        headers
+    }
 }
 const handleChat = ()=>{
     userMessege = chatInput.value.trim()
     if (!userMessege)return;
 
     Chatbox.appendChild(createChatLi(userMessege,"outgoing"));
+
+    setTimeout(()=>{
+        Chatbox.appendChild(createChatLi("Thinking...","incomming"));
+        generateResponce();
+    },600)
 }
-sendChatBtn.addEventListener("click", handleChat);
+sendChatBtn.addEventListener("click" ,handleChat)
