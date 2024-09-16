2684546005d07508e411161b2be5e307bc5937f6
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-16 22:28:23 +0530

    adding js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const sendChatBtn =document.querySelector(".chat-input span");
-const chatInput =document.querySelector(".chat-input textarea")
+const chatInput =document.querySelector(".chat-input textarea");
+const Chatbox=document.querySelector(".Chatbox")
 let userMessege;
+
+const createChatLi =(message ,className) =>{
+    const chatLi =document.createElement("li")
+    chatLi.classList.add("chat",className);
+    let chatContent =className === "outgoing" ?' <p>${message}</p> ':
+       ' <span className="material -symbols-outlined">smart_toy</span><p>${message}</p>';
+        chatLi.innerHTML=chatContent;
+        return chatLi
+}
 const handleChat = ()=>{
     userMessege = chatInput.value.trim()
-    console.log(userMessege)
+    if (!userMessege)return;
+
+    Chatbox.appendChild(createChatLi(userMessege,"outgoing"));
 }
-sendChatBtn.addEventListener("Click",handleChat)
+sendChatBtn.addEventListener("click",handleChat);
