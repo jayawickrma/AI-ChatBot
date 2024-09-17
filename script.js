dc0ab03b5b67e6b545c71be929c8dae1382c8f06
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-17 11:08:45 +0530

    done something have to do

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const ChatToggler=document.querySelector(".Chatbot-toggler");
 const chtbotCloseBtn =document.querySelector(".close-btn")
 let userMessege;
 
-const API_KEY ="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";
+const API_KEY ="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";
 const inputInitHeight =chatInput.scrollHeight;
 const createChatLi =(message ,className) =>{
     const chatLi =document.createElement("li")
     chatLi.classList.add("chat",className);
     let ChatContent =className === "outgoing" ?' <p></p> ':
-       ' <span class="material -symbols-outlined">smart_toy</span><p></p>';
-        chatLi.innerHTML=ChatContent;
-        chatLi.querySelector("p").textContent =message;
-        return chatLi;
+        ' <span class="material -symbols-outlined">smart_toy</span><p></p>';
+    chatLi.innerHTML=ChatContent;
+    chatLi.querySelector("p").textContent =message;
+    return chatLi;
 }
 const generateResponce =(incommingChatLi) =>{
     const API_URL = "https://api.openai.com/v1/chat/completions";
@@ -56,11 +56,12 @@ const handleChat = ()=>{
 }
 
 chatInput.addEventListener("keydown",(e)=>{
-   if (e.key ==="Enter" && window.innerWidth>800){
-       e.preventDefault();
-       handleChat();
-   }
+    if (e.key ==="Enter" && window.innerWidth>800){
+        e.preventDefault();
+        handleChat();
+    }
 });
+
 ChatToggler.addEventListener("click",()=>document.body.classList.toggle("show-Chatbot"));
 sendChatBtn.addEventListener("click" ,handleChat);
 chtbotCloseBtn.addEventListener("click",() =>document.body.classList.remove("show-Chatbot"))
